c638eadbae0c8d0834a861909e98097f4e7035a2
unknown <risnarendra@gmail.com>
2019-07-06 23:26:55 -0500

    Finished valid parentheses code

---
+++ ValidParentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.isEmpty()) return true;
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            char current = s.charAt(i);
+            if (current == '{' || current == '(' || current == '[')
+            {
+                stack.push(current);
+            }
+
+            if (current == '}' || current == ')' || current == ']')
+            {
+                if (stack.isEmpty()) return false;
+
+                char last = stack.peek();
+                if (current == '}' && last == '{' || current == ')' && last == '(' || current                   == ']' && last == '[') stack.pop();
+                else return false;
+            }
+    }
+    return stack.isEmpty();
+    }
+}
